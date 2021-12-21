1d1ee163d401b8411ac5c842c673d6e43a3d97de
Thomas Selden <thomasselden42@gmail.com>
2021-12-20 20:31:22 -0500

    fixing paths for ./controllers/places.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('controllers/places.js', require('controllers\places.js'))
+app.use('./places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
