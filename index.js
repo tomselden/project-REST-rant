c4afeb2fe97bd107e064d0da45d22c1ae14a6aec
Thomas Selden <thomasselden42@gmail.com>
2021-12-22 16:21:16 -0500

    adding 404

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.use('./places', require('./controllers/places'))
+app.set('view engine', 'jsx')
+app.engine('jsx', require('./express-react-views').createEngine())
+
+app.use('./places', require('.//controllers//places'))
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
