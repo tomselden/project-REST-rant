cad811ed4bf40787c335218fdaf22e595313b94a
Thomas Selden <thomasselden42@gmail.com>
2021-12-22 16:25:07 -0500

    Adding 404 page view

--- views/default.jsx
+++ views/default.jsx
@@ -1,16 +1,14 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-            </head>
-            <body>
-                {html.children}
-            </body>
-        </html>
-    )
-  }
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>Title</title>
+      </head>
+      <body>{html.children}</body>
+    </html>
+  );
+}
 
-module.exports = Def
+module.exports = Def;
--- views/home.jsx
+++ views/home.jsx
@@ -1,15 +1,14 @@
-const React = require('react')
-const Def = require('./default')
+const React = require("react");
+const Def = require("./default");
 
-function home () {
-    return (
-      <Def>
-          <main>
-              <h1>HOME</h1>
-          </main>
-      </Def>
-    )
-  }
+function home() {
+  return (
+    <Def>
+      <main>
+        <h1>HOME</h1>
+      </main>
+    </Def>
+  );
+}
 
-module.exports = home
-
+module.exports = home;
