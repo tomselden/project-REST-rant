3baaa3768c8f82b92b8a12428f62a6b1b7d1a5bb
Thomas Selden <thomasselden42@gmail.com>
2021-12-22 16:32:40 -0500

    Adding places view with mock data

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const app = express();
 
-app.set('view engine', 'jsx')
-app.engine('jsx', require('./express-react-views').createEngine())
+app.set("view engine", "jsx");
+app.engine("jsx", require("./express-react-views").createEngine());
 
-app.use('./places', require('.//controllers//places'))
+app.use("./places", require(".//controllers//places"));
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
+// GET /places
+app.get("/", (req, res) => {
+  let places = [];
+  res.render('places/index', { places })
+});
 
-app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-})
+let places = [
+  {
+    name: "H-Thai-ML",
+    city: "Seattle",
+    state: "WA",
+    cuisines: "Thai, Pan-Asian",
+    pic: "http://placekitten.com/250/250",
+  },
+  {
+    name: "Coding Cat Cafe",
+    city: "Phoenix",
+    state: "AZ",
+    cuisines: "Coffee, Bakery",
+    pic: "http://placekitten.com/250/250",
+  },
+];
 
-app.listen(process.env.PORT)
+app.get("*", (req, res) => {
+  res.status(404).send("<h1>404 Page</h1>");
+});
+
+app.listen(process.env.PORT);
