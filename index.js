746477ac930f6922f12946f4d7880d30e7d304aa
Thomas Selden <thomasselden42@gmail.com>
2022-01-03 17:26:23 -0500

    Places not getting through, error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
 
 app.use("./places", require("./controllers/places"));
+app.use(express.urlencoded({ extended: true }))
 
 let places = [
   {
