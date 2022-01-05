495094a41cf71cb204d3c51302059c28fd7cf86a
Thomas Selden <thomasselden42@gmail.com>
2022-01-05 14:02:35 -0500

    Navbar with style

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const router = require('express').Router()
-const places = require('../models/places.js')
+const places = require('./models/places.js')
 
 router.get('/', (req, res) => {
     res.render('places/index', { places })
@@ -14,23 +14,6 @@ app.engine("jsx", require("express-react-views").createEngine());
 
 app.use("./places", require("./controllers/places"));
 app.use(express.urlencoded({ extended: true }))
-
-let places = [
-  {
-    name: "H-Thai-ML",
-    city: "Seattle",
-    state: "WA",
-    cuisines: "Thai, Pan-Asian",
-    pic: "http://placekitten.com/250/250",
-  },
-  {
-    name: "Coding Cat Cafe",
-    city: "Phoenix",
-    state: "AZ",
-    cuisines: "Coffee, Bakery",
-    pic: "http://placekitten.com/250/250",
-  },
-];
 
 app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
--- views/default.jsx
+++ views/default.jsx
@@ -3,10 +3,23 @@ const React = require("react");
 function Def(html) {
   return (
     <html>
-      <head>
-        <title>Title</title>
-      </head>
-      <body>{html.children}</body>
+      <head>...</head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+      </body>
     </html>
   );
 }
